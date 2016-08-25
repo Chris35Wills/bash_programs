a51629d5b4c6f9abad993f579cee57749844d190
CN Williams <cw14910@bristol.ac.uk>
2016-08-25 16:57:14 +0100

    csv2shp

--- bulk_csv_to_shp.sh
+++ bulk_csv_to_shp.sh
@@ -18,41 +18,56 @@
 # see: http://stackoverflow.com/questions/7529856/bash-getopts-retrieving-multiple-variables-from-one-flag
 # see: http://wiki.bash-hackers.org/howto/getopts_tutorial
 # see: http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
-while getopts ":s:x:y:p" opt; do
-  case $opt in
-  	s)
-      echo "-search_string was triggered!" #>&2
-      search_string="$OPTARG" # set variable from option...
-      ;;
-    x)
-      echo "-x flag triggered!" #>&2
-      x_col_name="$OPTARG" # set variable from option...
-      ;;
-    y)
-      echo "-y flag triggered!" #>&2
-      y_col_name="$OPTARG" # set variable from option...
-      ;;
-    p)
-      echo "-proj flag triggered!" #>&2
-      proj_string="$OPTARG" # modify to accept gaps
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" #>&2
-      ;;
-  esac
-done
-
-echo "Variables as passed in:"
-echo "File search string: " $search_string
-echo "x coordinate column header: " $x_col_name
-echo "y coordinate column header: "$y_col_name
-echo "PROJ4 string: " $proj_string
-
-echo $proj_string
-
-exit
-
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< FIX VARIABLE READ IN
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#while getopts ":s:x:y:p" opt; do
+#  case $opt in
+#  	s)
+#      echo "-search_string was triggered!" #>&2
+#      search_string="$OPTARG" # set variable from option...
+#      ;;
+#    x)
+#      echo "-x flag triggered!" #>&2
+#      x_col_name="$OPTARG" # set variable from option...
+#      ;;
+#    y)
+#      echo "-y flag triggered!" #>&2
+#      y_col_name="$OPTARG" # set variable from option...
+#      ;;
+#    p)
+#      echo "-proj flag triggered!" #>&2
+#      proj_string="$OPTARG" # modify to accept gaps
+#      ;;
+#    \?)
+#      echo "Invalid option: -$OPTARG" #>&2
+#      ;;
+#  esac
+#done
+#echo "Variables as passed in:"
+#echo "File search string: " $search_string
+#echo "x coordinate column header: " $x_col_name
+#echo "y coordinate column header: "$y_col_name
+#echo "PROJ4 string: " $proj_string
+#
+#echo $proj_string
+#
+#exit
+#<<<<<<<<<<<<<<<<<
+#<<<<<<<<<<<<<<<<<<<<< END OF ISSUE
+#<<<<<<<<<<<<<<<<<
 ##~~~~~~~~~~~~
+
+#<<<<<<<<<<<<<<<<<<
+#<<<<<<<<<<<<<<<<<<<<< REPLACE WITH COMMAND LINE OPTIONS ABOVE
+#<<<<<<<<<<<<<<<<<<
+search_string='C:/GitHub/synthetic_channels/FENTY_DEM_along_track_FT/run_3/paths/*.csv'
+x_col_name='coord_x'
+y_col_name='coord_y'
+proj_string='+proj=stere +lat_0=90 +lat_ts=71 +lon_0=-39 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
+#<<<<<<<<<<<<<<<<<<
+#<<<<<<<<<<<<<<<<<<<<< END OF ISSUE
+#<<<<<<<<<<<<<<<<<<
 
 FILES=$search_string
 
